a1a98dcd94ea2115218d9b0d51a5054052338dc1
Emily Bates <elbates@uvm.edu>
2014-11-30 16:21:14 -0500

    initial commit

---
+++ head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Emily Bates">
+        <meta name="description" content="Bottoms Up!">
+        
+        <title>Bottoms Up!</title>
+        <link href="style.css" type="text/css" rel="stylesheet" />
+    </head>
+    <nav>
+        <ol>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="signup.php">SIGN UP</a></li>
+            <li><a href="feedback.php">FEEDBACK</a></li>
+        </ol>
+    </nav>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require("head.php");?>
+<body>
+    <p>This is a test!</p>
+</body>
+
